99778d134ada63d710e8980c41f40f52fc3885c8
ShesmanRenato <shesmanrenato@MacSrsg.local>
2021-06-25 14:01:20 -0300

    Corecao de alguns valores e de singular e plural

--- valorExtenso.js
+++ valorExtenso.js
@@ -18,6 +18,7 @@ function vExtenso(vlr) {
             var auxnumero;
             var auxnumero2;
             var auxnumero3;
+            var aux;
 
             var unidade = ["", "um", "dois", "três", "quatro", "cinco",
                 "seis", "sete", "oito", "nove", "dez", "onze",
@@ -31,7 +32,7 @@ function vExtenso(vlr) {
             var dezena = ["", "", "vinte", "trinta", "quarenta", "cinquenta",
                 "sessenta", "setenta", "oitenta", "noventa"];
 
-            var qualificaS = ["um real", "mil", "milhão", "bilhão", "trilhão"];
+            var qualificaS = ["reais", "mil", " milhão", "bilhão", "trilhão"];
             var qualificaP = ["reais", "mil", "milhões", "bilhões", "trilhões"];
 
             for (var i = cont; i > 0; i--) {
@@ -44,6 +45,8 @@ function vExtenso(vlr) {
                 auxnumero2 = vlrS.substr(cont - i, 1);
                 auxnumero = parseInt(auxnumero2);
 
+                aux = vlrS.substr(cont - i + 1, 2);
+
 
                 if ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2)) {
                     auxnumero2 = vlrS.substr(cont - i, 2);
@@ -51,12 +54,25 @@ function vExtenso(vlr) {
                 }
 
                 if ((i == 15) || (i == 12) || (i == 9) || (i == 6) || (i == 3)) {
-                    extenso = extenso + centena[auxnumero];
-                    auxnumero2 = vlrS.substr(cont - i + 1, 1)
-                    auxnumero3 = vlrS.substr(cont - i + 2, 1)
+                   
+                    if ((auxnumero == 1) && (aux == "00")) {
 
-                    if ((auxnumero2 != "0") || (auxnumero3 != "0"))
-                        extenso += " e ";
+                        extenso = extenso + "Cem";
+                        auxnumero2 = vlrS.substr(cont - i + 1, 1)
+                        auxnumero3 = vlrS.substr(cont - i + 2, 1)
+
+                        if ((auxnumero2 != "0") || (auxnumero3 != "0"))
+                            extenso += " e ";
+
+                    } else {
+                        extenso = extenso + centena[auxnumero];
+                        auxnumero2 = vlrS.substr(cont - i + 1, 1)
+                        auxnumero3 = vlrS.substr(cont - i + 2, 1)
+
+                        if ((auxnumero2 != "0") || (auxnumero3 != "0"))
+                            extenso += " e ";
+                    }
+
 
                 } else
 
@@ -72,9 +88,23 @@ function vExtenso(vlr) {
                     else if ((auxnumero <= 19) && (auxnumero > 9) && ((i == 14) || (i == 11) || (i == 8) || (i == 5) || (i == 2))) {
                         extenso = extenso + unidade[auxnumero];
                     } else if ((auxnumero < 10) && ((i == 13) || (i == 10) || (i == 7) || (i == 4) || (i == 1))) {
-                        auxnumero3 = vlrS.substr(cont - i - 1, 1);
-                        if ((auxnumero3 != "1") && (auxnumero3 != ""))
-                            extenso = extenso + unidade[auxnumero];
+                        auxnumero3 = vlrS.substr(cont - i, 1);
+                        // console.log(auxnumero3);
+                        if ((auxnumero3 != "1") && (auxnumero3 != "")) {
+                            // console.log(auxnumero);
+                            ; extenso = extenso + unidade[auxnumero];
+                            // console.log("teste");
+                        }
+                        else if (auxnumero3 == "1") {
+                            if (cont == 1) {
+                                extenso = extenso + "um real"
+                            } else {
+                                //console.log(auxnumero);
+                                extenso = extenso + unidade[auxnumero];
+                            }
+
+                        }
+
                     }
 
                 if (i % 3 == 1) {
@@ -108,18 +138,29 @@ function vExtenso(vlr) {
                     if (i == 4) {
                         if (verifica2 == 1) {
                             extenso = extenso + " " + qualificaS[1] + " ";
-                        } else if (verifica2 != 0) { extenso = extenso + " " + qualificaP[1] + " "; }
+                            //console.log("valor do if:" + extenso);
+                        } else if (verifica2 != 0) {
+                            extenso = extenso + " " + qualificaP[1] + " ";
+                            //console.log("valor do else:" + extenso);
+                        }
                     }
                     if (i == 1) {
-                        if (verifica2 == 1) {
-                            extenso = extenso + " " + qualificaS[0] + " ";
-                        } else { extenso = extenso + " " + qualificaP[0] + " "; }
+                        if ((cont == 1) && (extenso == "um real")) {
+                            extenso = extenso
+                        } else {
+                            if (verifica2 == 1) {
+                                extenso = extenso + " " + qualificaS[0] + " ";
+                            } else { extenso = extenso + " " + qualificaP[0] + " "; }
+                        }
                     }
                 }
             }
             resto = resto * 100;
             var aexCent = 0;
-            if (resto <= 19 && resto > 0)
+            if (resto == 1) {
+                extenso += " e " + unidade[resto] + " centavo"
+            }
+            if (resto <= 19 && resto > 1)
                 extenso += " e " + unidade[resto] + " Centavos";
             if (resto > 19) {
                 aexCent = parseInt(resto / 10);
@@ -139,4 +180,4 @@ function vExtenso(vlr) {
     return valorExtenso;
 }
 
-console.log(vExtenso("1.01"))
+console.log(vExtenso("101"))
